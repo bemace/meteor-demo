e0461b14b17f164ada754e406b62136599585f03
Brad Mace <bemace@gmail.com>
2015-10-25 17:40:02 -0500

    updating contacts works

--- client/template-helpers.js
+++ client/template-helpers.js
@@ -1,16 +1,7 @@
-if (Meteor.isClient) {
-  // counter starts at 0
-  Session.setDefault('counter', 0);
-
   Template.contactList.helpers({
     contacts: function() {
       return Contacts.find();
     }
   });
-}
 
-if (Meteor.isServer) {
-  Meteor.startup(function () {
-    // code to run on server at startup
-  });
-}
+  
---
+++ client/templates/editContact.js
@@ -0,0 +1,39 @@
+Template.editContact.events({
+  'submit form': function (e, tmpl) {
+    e.preventDefault();
+    var form = e.target;
+    var user = Meteor.user();
+    var _this = this;
+
+    if (this._id) {
+      Contacts.update(this._id, { $set: {
+        name: form.name.value,
+        email: form.email.value,
+        phone: form.phone.value,
+      }}, 
+      function(error) {
+        if (error)
+          alert(error.reason);
+        else
+          Router.go('Contact', {slug: _this._id});
+      });
+    }
+    else {
+      Meteor.call('insertContact', {
+        name:      form.name.value,
+        email:  form.email.value,
+        phone:       form.phone.value,
+
+      }, function(error, newId) {
+        Session.set('saveButton', 'Save Contact');
+
+        if(error) {
+          return alert(error.reason);
+        }
+
+        Router.go('Contact', {slug: newId});
+      });
+
+    }
+  }
+});
