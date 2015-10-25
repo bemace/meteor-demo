56492c056eff036d9a171a2e83967d9462ad2a44
Brad Mace <bemace@gmail.com>
2015-10-25 14:59:18 -0500

    auto-add dummy contacts to db; display contacts from db

--- demo.js
+++ demo.js
@@ -2,25 +2,9 @@ if (Meteor.isClient) {
   // counter starts at 0
   Session.setDefault('counter', 0);
 
-  Template.hello.helpers({
-    counter: function () {
-      return Session.get('counter');
-    }
-  });
-
-  Template.hello.events({
-    'click button': function () {
-      // increment the counter when button is clicked
-      Session.set('counter', Session.get('counter') + 1);
-    }
-  });
-
   Template.contactList.helpers({
     contacts: function() {
-      return [
-        { name: 'John Doe', email: 'johndoe@gmail.com', phone: '320-555-1234' },
-        { name: 'Jane Doe', email: 'janedoe@gmail.com', phone: '320-555-4321' },
-      ];
+      return Contacts.find();
     }
   });
 }
